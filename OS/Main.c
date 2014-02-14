445054c82165efb6ae4d51ad3f0e41bb1a3c02b6
branmart <branmart@uw.edu>
2014-02-13 16:10:31 -0800

    Init commit

---
+++ OS/Main.c
@@ -0,0 +1,51 @@
+/*
+ * Main.c
+ *
+ *  Created on: Feb 11, 2014
+ *      Author: Brandon M Martin
+ */
+
+
+
+#include<stdio.h>
+/*
+ * Simple output
+ *
+ */
+main() {
+
+
+
+	printf("Scheduler: ");
+	printf("Running process <Insert Processes> next.");
+	//TODO insert process
+
+	printf("\n\tProcess 0 (Calculator)'s state <Insert State>");
+	//TODO insert state
+	printf("\n\tProcess 2 (Consumer)'s state <Insert State>");
+	//TODO insert state
+	printf("\n\tProcess 3 (Producer)'s state <Insert State>");
+	//TODO insert state
+
+	printf("\n<Insert Interrupt>");
+	//TODO insert what interrupted
+
+	cpu();
+}
+
+
+cpu() {
+
+	printf("cpu");
+
+}
+
+
+processes() {
+
+}
+
+scheduler() {
+
+}
+
